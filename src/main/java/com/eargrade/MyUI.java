39a64b72a7a49fcb4b7c120fe09af9bdd18b74bd
Havlong <grishin_evgenii@inbox.ru>
2017-10-28 18:02:00 +0400

    second commit

--- src/main/java/com/eargrade/MyUI.java
+++ src/main/java/com/eargrade/MyUI.java
@@ -4,13 +4,15 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FileResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,18 +26,47 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        VerticalLayout layout = new VerticalLayout();
+        HorizontalLayout info = new HorizontalLayout();
+        String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+        FileResource resource = new FileResource(new File(basepath + "/WEB-INF/classes/original.jpg"));
+        Image logo = new Image("", resource);
+        Label companyName = new Label("EarGrade");
+        info.addComponents(logo, companyName);
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        HorizontalLayout authorize = new HorizontalLayout();
+        VerticalLayout form = new VerticalLayout();
+        TextField username = new TextField("Username");
+        final TextField yourId = new TextField("Enter ID of your EarGrade");
+        yourId.setVisible(false);
+        Button log = new Button("Log in");
+        log.addClickListener(e -> {
+            //TODO authorize
+            //if don't have a username for any id
+            yourId.setVisible(true);
+            //TODO register new user
         });
-        
-        layout.addComponents(name, button);
+        form.addComponents(username, yourId);
+        authorize.addComponents(form, log);
+
+        HorizontalLayout melodying = new HorizontalLayout();
+        Button download = new Button("Download song");
+        download.addClickListener(e->{
+            //TODO set last melody on chosen and send melody
+        });
+        Label difficulty = new Label("No melody had been chosen");
+        ComboBox<String> melody = new ComboBox<>("Choose melody");
+        ArrayList<String> melodyStructure = new ArrayList<>();
+        Collections.addAll(melodyStructure, "Tetris", "Snowy Tree");
+        melody.setItems(melodyStructure);
+        melody.addSelectionListener(e ->{
+            //TODO correct scoreboard via data, set difficulty of current melody
+        });
+        melodying.addComponents(melody, download, difficulty);
+
+
+
+        layout.addComponents(info, authorize, melodying);
         
         setContent(layout);
      }
