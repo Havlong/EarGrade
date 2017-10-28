55379d6729d654eebcda84f4cfbec4fc088a7055
Havlong <grishin_evgenii@inbox.ru>
2017-10-28 23:10:57 +0400

    third commit

--- src/main/java/com/eargrade/MyUI.java
+++ src/main/java/com/eargrade/MyUI.java
@@ -11,8 +11,7 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -29,9 +28,10 @@ public class MyUI extends UI {
         VerticalLayout layout = new VerticalLayout();
         HorizontalLayout info = new HorizontalLayout();
         String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-        FileResource resource = new FileResource(new File(basepath + "/WEB-INF/classes/original.jpg"));
+        FileResource resource = new FileResource(new File(basepath + "/WEB-INF/classes/original.png"));
         Image logo = new Image("", resource);
-        Label companyName = new Label("EarGrade");
+        logo.setWidth("200px");logo.setHeight("120px");
+        Label companyName = new Label("EarGrade - your way to the music");
         info.addComponents(logo, companyName);
 
         HorizontalLayout authorize = new HorizontalLayout();
@@ -54,25 +54,48 @@ public class MyUI extends UI {
         download.addClickListener(e->{
             //TODO set last melody on chosen and send melody
         });
-        Label difficulty = new Label("No melody had been chosen");
         ComboBox<String> melody = new ComboBox<>("Choose melody");
         ArrayList<String> melodyStructure = new ArrayList<>();
         Collections.addAll(melodyStructure, "Tetris", "Snowy Tree");
         melody.setItems(melodyStructure);
         melody.addSelectionListener(e ->{
-            //TODO correct scoreboard via data, set difficulty of current melody
+            //TODO correct scoreboard via data
         });
-        melodying.addComponents(melody, download, difficulty);
+        melodying.addComponents(melody, download);
 
 
+        List<Record> scoreboard = Arrays.asList(new Record("Havlong", 100)
+                , new Record("Mapk58", 20)
+                , new Record("MrHamsterGG", 80));
+        Grid<Record> board = new Grid<>();
+        board.setItems(scoreboard);
+        board.addColumn(Record::getUsername).setCaption("User");
+        board.addColumn(Record::getScore).setCaption("Record");
 
-        layout.addComponents(info, authorize, melodying);
-        
+        layout.addComponents(info, authorize, melodying, board);
+
         setContent(layout);
      }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
+    }
+    private class Record{
+        String username;
+        int score;
+
+        public Record(String username, int score){
+            this.username = username;
+            this.score = score;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public String getUsername() {
+            return username;
+        }
     }
 }
